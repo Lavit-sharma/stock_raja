36a85a4ab21d1dac2cd54bf16b7989dafd1d8fb4
LAVIT SHARMA <159767347+Lavit-sharma@users.noreply.github.com>
2026-04-16 09:06:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const scrapeJobs = require("./scraper");
 const writeToSheet = require("./sheets");
+const pLimit = require("p-limit");
+
+const limit = pLimit(2); // keyword parallel
 
 const keywords = [
   "developer",
@@ -11,7 +14,6 @@ const keywords = [
   "frontend",
   "backend"
 ];
-
 const locations = [
   "delhi",
   "mumbai",
@@ -23,28 +25,26 @@ const locations = [
 ];
 
 (async () => {
-  let finalJobs = [];
-  const seen = new Set();
+  const tasks = [];
 
   for (let keyword of keywords) {
     for (let location of locations) {
-      const jobs = await scrapeJobs(keyword, location, 5); // more pages
+      tasks.push(
+        limit(async () => {
+          const jobs = await scrapeJobs(keyword, location, 5);
 
-      const enriched = jobs.map(j => ({
-        ...j,
-        keyword,
-        searchLocation: location
-      }));
-
-      const unique = enriched.filter(j => {
-        if (!j.link || seen.has(j.link)) return false;
-        seen.add(j.link);
-        return true;
-      });
-
-      finalJobs.push(...unique);
+          return jobs.map(j => ({
+            ...j,
+            keyword,
+            searchLocation: location
+          }));
+        })
+      );
     }
   }
+
+  const results = await Promise.all(tasks);
+  const finalJobs = results.flat();
 
   await writeToSheet(finalJobs);
 })();

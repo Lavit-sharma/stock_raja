766ed7a30df3d39d3a13946e3286ee3cc8899093
LAVIT SHARMA <159767347+Lavit-sharma@users.noreply.github.com>
2026-05-08 09:09:30 +0530

    Update screen.py

--- screen.py
+++ screen.py
@@ -239,7 +239,7 @@ def get_driver():
 
 def inject_tv_cookies(driver):
     try:
-        cookie_data = os.getenv("TRADINGVIEW_COOKIES2")
+        cookie_data = os.getenv("TRADINGVIEW_COOKIES")
         if not cookie_data:
             log("❌ TRADINGVIEW_COOKIES missing.")
             return False
